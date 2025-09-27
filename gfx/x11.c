9618f2e7a437c60dba79403c681ecd5494c5b6d3
Antoni Sawicki <as@tenoware.com>
2025-09-27 02:28:12 -0700

    fix includes

--- gfx/x11.c
+++ gfx/x11.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE
 #include "../graphics.h"
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/Xatom.h>
 #include <X11/keysym.h>
 #include <sys/time.h>
 #include <sys/select.h>
